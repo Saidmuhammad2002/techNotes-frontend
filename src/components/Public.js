bc1441426d1f5456206cdfb1a95a6ac1d20c061d
Saidmuhammad2002 <sololearner567@gmail.com>
2023-05-18 20:50:54 +0500

    latest change

--- src/components/Public.js
+++ src/components/Public.js
@@ -1,28 +1,35 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Public = () => {
-    const content = (
-        <section className="public">
-            <header>
-                <h1>Welcome to <span className="nowrap">Dan D. Repairs!</span></h1>
-            </header>
-            <main className="public__main">
-                <p>Located in Beautiful Downtown Foo City, Dan D. Repairs  provides a trained staff ready to meet your tech repair needs.</p>
-                <address className="public__addr">
-                    Dan D. Repairs<br />
-                    555 Foo Drive<br />
-                    Foo City, CA 12345<br />
-                    <a href="tel:+15555555555">(555) 555-5555</a>
-                </address>
-                <br />
-                <p>Owner: Dan Davidson</p>
-            </main>
-            <footer>
-                <Link to="/login">Employee Login</Link>
-            </footer>
-        </section>
-
-    )
-    return content
-}
-export default Public
+  const content = (
+    <section className="public">
+      <header>
+        <h1>
+          Welcome to <span className="nowrap">Saidmuhammad Tech Company</span>
+        </h1>
+      </header>
+      <main className="public__main">
+        <p>
+          Located in Beautiful Tashkent City, Saidmuhammad B. provides a trained
+          staff ready to meet your tech repair needs.
+        </p>
+        <address className="public__addr">
+          Saidmuhammad Tech
+          <br />
+          555 Tashkent Drive
+          <br />
+          Tashkent, Uzbekistan 12345
+          <br />
+          <a href="tel:+998885506505">+998 (88) 550-65-05</a>
+        </address>
+        <br />
+        <p>Owner: Saidmuhammad Boboyev</p>
+      </main>
+      <footer>
+        <Link to="/login">Employee Login</Link>
+      </footer>
+    </section>
+  );
+  return content;
+};
+export default Public;
